97ad5c89708bde4bce9a491c76b364c4bd303cfc
Fredrik Ekelund <fredrik@fredrik.computer>
2018-01-31 09:54:28 +0100

    Install Puppet quietly

--- files/scripts/install-puppet.sh
+++ files/scripts/install-puppet.sh
@@ -3,8 +3,8 @@
 cd /tmp
 wget http://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 dpkg -i puppetlabs-release-pc1-xenial.deb
-apt-get update
-apt-get -y install puppet-common
+apt-get -q update
+apt-get -qy install puppet-common
 echo "PATH=/opt/puppetlabs/bin:$PATH" >> /etc/bash.bashrc
 echo "export PATH" >> /etc/bash.bashrc
 export PATH=/opt/puppetlabs/bin:$PATH
