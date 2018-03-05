17e9e2454e1ceb480ea8e01d5e59df917df2788b
Fredrik Ekelund <fredrik@lundgrenlindqvist.se>
2018-03-05 14:51:35 +0100

    Fix path to phpMyAdmin folder

--- puppet/modules/phpmyadmin/manifests/init.pp
+++ puppet/modules/phpmyadmin/manifests/init.pp
@@ -5,24 +5,24 @@ class phpmyadmin::install {
   exec { 'download-phpmyadmin':
     command => '/usr/bin/wget https://files.phpmyadmin.net/phpMyAdmin/4.7.7/phpMyAdmin-4.7.7-all-languages.tar.gz',
     cwd     => '/vagrant/',
-    creates => '/vagrant/phpMyAdmin-4.7.1-all-languages.zip'
+    creates => '/vagrant/phpMyAdmin-4.7.7-all-languages.zip'
   }
 
   exec { 'untar-phpmyadmin':
     cwd     => '/vagrant/',
-    command => '/bin/tar xzf /vagrant/phpMyAdmin-4.7.1-all-languages.tar.gz',
+    command => '/bin/tar xzf /vagrant/phpMyAdmin-4.7.7-all-languages.tar.gz',
     require => Exec['download-phpmyadmin'],
-    creates => '/vagrant/phpMyAdmin-4.7.1-all-languages',
+    creates => '/vagrant/phpMyAdmin-4.7.7-all-languages',
   }->
 
   file { '/vagrant/phpmyadmin':
     ensure => 'directory',
     recurse => true,
-    source => 'file:///vagrant/phpMyAdmin-4.7.1-all-languages',
-    before => File['/vagrant/phpMyAdmin-4.7.1-all-languages'],
+    source => 'file:///vagrant/phpMyAdmin-4.7.7-all-languages',
+    before => File['/vagrant/phpMyAdmin-4.7.7-all-languages'],
   }->
 
-  file { '/vagrant/phpMyAdmin-4.7.1-all-languages':
+  file { '/vagrant/phpMyAdmin-4.7.7-all-languages':
     ensure => 'absent',
     purge => true,
     recurse => true,
