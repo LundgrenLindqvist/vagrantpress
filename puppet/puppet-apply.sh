0f7f77d297f9de69a67314b7709f5177ba57b9ba
Fredrik Ekelund <fredrik@lundgrenlindqvist.se>
2018-03-08 17:13:06 +0100

    Updated puppet-apply.sh

--- puppet/puppet-apply.sh
+++ puppet/puppet-apply.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
+
+if [ ! -e puppet-release-xenial.deb ]; then
+  wget -q http://apt.puppetlabs.com/puppet-release-xenial.deb
+  dpkg -i puppet-release-xenial.deb
+  apt-get update
+  apt-get -qy install puppet-agent
+fi
+
+export PATH=/opt/puppetlabs/bin:$PATH
+
+puppet module install flypenguin-mkdir --target-dir modules --version 1.0.4
+puppet module install hunner-wordpress --target-dir modules --version 1.0.0
+puppet module install puppetlabs-mysql --target-dir modules --version 5.3.0
+
 puppet apply --modulepath=modules --hiera_config=hiera.yaml manifests/init.pp
