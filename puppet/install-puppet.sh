45371c2cbbb9432f6f863652fb54c5ae7f4a9671
Fredrik Ekelund <fredrik@lundgrenlindqvist.se>
2018-03-08 16:54:06 +0100

    Use Puppet Hiera for storing config values

--- puppet/install-puppet.sh
+++ puppet/install-puppet.sh
@@ -11,5 +11,6 @@ fi
 
 export PATH=/opt/puppetlabs/bin:$PATH
 
+puppet module install flypenguin-mkdir --target-dir /vagrant/puppet/modules --version 1.0.4
 puppet module install hunner-wordpress --target-dir /vagrant/puppet/modules --version 1.0.0
 puppet module install puppetlabs-mysql --target-dir /vagrant/puppet/modules --version 5.3.0
--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -27,6 +27,101 @@ APT::Periodic::AutocleanInterval "7";
 APT::Periodic::Unattended-Upgrade "1";
 END
 
+$sshd_config_template = @(END)
+# Package generated configuration file
+# See the sshd_config(5) manpage for details
+
+# What ports, IPs and protocols we listen for
+Port 22
+# Use these options to restrict which interfaces/protocols sshd will bind to
+#ListenAddress ::
+#ListenAddress 0.0.0.0
+Protocol 2
+# HostKeys for protocol version 2
+HostKey /etc/ssh/ssh_host_rsa_key
+HostKey /etc/ssh/ssh_host_dsa_key
+HostKey /etc/ssh/ssh_host_ecdsa_key
+HostKey /etc/ssh/ssh_host_ed25519_key
+#Privilege Separation is turned on for security
+UsePrivilegeSeparation yes
+
+# Lifetime and size of ephemeral version 1 server key
+KeyRegenerationInterval 3600
+ServerKeyBits 1024
+
+# Logging
+SyslogFacility AUTH
+LogLevel INFO
+
+# Authentication:
+LoginGraceTime 120
+PermitRootLogin no
+StrictModes yes
+
+RSAAuthentication yes
+PubkeyAuthentication yes
+#AuthorizedKeysFile	%h/.ssh/authorized_keys
+
+# Don't read the user's ~/.rhosts and ~/.shosts files
+IgnoreRhosts yes
+# For this to work you will also need host keys in /etc/ssh_known_hosts
+RhostsRSAAuthentication no
+# similar for protocol version 2
+HostbasedAuthentication no
+# Uncomment if you don't trust ~/.ssh/known_hosts for RhostsRSAAuthentication
+#IgnoreUserKnownHosts yes
+
+# To enable empty passwords, change to yes (NOT RECOMMENDED)
+PermitEmptyPasswords no
+
+# Change to yes to enable challenge-response passwords (beware issues with
+# some PAM modules and threads)
+ChallengeResponseAuthentication no
+
+# Change to no to disable tunnelled clear text passwords
+#PasswordAuthentication yes
+
+# Kerberos options
+#KerberosAuthentication no
+#KerberosGetAFSToken no
+#KerberosOrLocalPasswd yes
+#KerberosTicketCleanup yes
+
+# GSSAPI options
+#GSSAPIAuthentication no
+#GSSAPICleanupCredentials yes
+
+X11Forwarding yes
+X11DisplayOffset 10
+PrintMotd no
+PrintLastLog yes
+TCPKeepAlive yes
+#UseLogin no
+
+#MaxStartups 10:30:60
+#Banner /etc/issue.net
+
+# Allow client to pass locale environment variables
+AcceptEnv LANG LC_*
+
+Subsystem sftp /usr/lib/openssh/sftp-server
+
+# Set this to 'yes' to enable PAM authentication, account processing,
+# and session processing. If this is enabled, PAM authentication will
+# be allowed through the ChallengeResponseAuthentication and
+# PasswordAuthentication.  Depending on your PAM configuration,
+# PAM authentication via ChallengeResponseAuthentication may bypass
+# the setting of "PermitRootLogin without-password".
+# If you just want the PAM account and session checks to run without
+# PAM authentication, then enable this but set PasswordAuthentication
+# and ChallengeResponseAuthentication to 'no'.
+UsePAM yes
+UseDNS no
+GSSAPIAuthentication no
+END
+
+$web_hostname = lookup('hostname')
+
 package { [
   'fail2ban',
   'git',
@@ -58,56 +153,37 @@ exec { 'ufw-enable':
 file { '/etc/apt/apt.conf.d/10periodic':
   ensure => file,
   content => inline_template($unattended_upgrades_template),
-  owner => 'root',
-  group => 'root'
+}
+
+file { '/etc/ssh/sshd_config':
+  ensure => file,
+  content => inline_template($sshd_config_template),
 }
 
 user { 'll':
   ensure => 'present',
   home => '/home/ll',
   groups => 'sudo',
-  password => 'dumbpass',
+  password => lookup('ll_password'),
   shell => '/bin/bash',
 }
 
 user { 'www-data':
   ensure => 'present',
   home => '/var/www',
-  password => 'dumbpass',
+  password => lookup('www_data_password'),
   shell => '/bin/bash',
 }
 
-file { [
-  '/var/www',
-  '/var/www/vagrantpress.test',
-  ]:
-  ensure => 'directory',
-  recurse => true,
+mkdir::p { lookup('nginx_root'):
   owner => 'www-data',
-  group => 'www-data'
-}
-
-class { 'nginx::install':
-  web_hostname => 'vagrantpress.test',
-  web_root => '/var/www/vagrantpress.test',
-  no_sendfile => true
-}
-
-class { 'php7::install': }
-
-class { 'mysql::server':
-  root_password => 'strongpassword',
-  remove_default_accounts => true
-}
-
-class { 'wordpress':
-  wp_owner => 'www-data',
-  wp_group => 'www-data',
-  db_user => 'wordpress',
-  db_password => 'hvyH(S%t(016',
-  install_dir => '/var/www/vagrantpress.test/wordpress',
-  wp_site_domain => 'http://vagrantpress.test',
-  version => '4.9'
+  group => 'www-data',
+  before => [
+    Class['nginx::install'],
+    Class['wordpress'],
+    Class['phpmyadmin::install'],
+    File['/var/www/vagrantpress.test/index.php']
+  ]
 }
 
 file { '/var/www/vagrantpress.test/index.php':
@@ -117,9 +193,32 @@ file { '/var/www/vagrantpress.test/index.php':
   group => 'www-data'
 }
 
+class { 'nginx::install':
+  web_hostname => $web_hostname,
+  web_root => lookup('nginx_root'),
+  no_sendfile => true
+}
+
+class { 'php7::install': }
+
+class { 'mysql::server':
+  root_password => lookup('mysql_root_password'),
+  remove_default_accounts => true
+}
+
+class { 'wordpress':
+  wp_owner => 'www-data',
+  wp_group => 'www-data',
+  db_user => 'wordpress',
+  db_password => lookup('mysql_wordpress_password'),
+  install_dir => lookup('wordpress_root'),
+  wp_site_domain => "http://${web_hostname}",
+  version => lookup('wordpress_version')
+}
+
 class { 'phpmyadmin::install':
-  version => '4.7.9',
-  install_dir => '/var/www/vagrantpress.test/phpmyadmin',
+  version => lookup('phpmyadmin_version'),
+  install_dir => lookup('phpmyadmin_root'),
   owner => 'www-data',
   group => 'www-data'
 }
