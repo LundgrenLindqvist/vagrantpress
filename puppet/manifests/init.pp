cc7903492f2b25750f1ecf7bb0b5d4dc96bcf662
Fredrik Ekelund <fredrik@lundgrenlindqvist.se>
2018-03-12 15:05:58 +0100

    Use file_line class from stdlib instead of complete php.ini template

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -50,7 +50,7 @@ $ports.each |String $port| {
 
 service { 'ufw':
   ensure => running,
-}->
+} ->
 
 exec { 'ufw-enable':
   command => 'echo "y" | ufw enable',
@@ -63,7 +63,6 @@ file { '/etc/apt/apt.conf.d/10periodic':
 }
 
 file_line { 'ssh_no_root_login':
-  ensure => present,
   path => '/etc/ssh/sshd_config',
   line => 'PermitRootLogin no',
   match => '^PermitRootLogin',
@@ -109,7 +108,35 @@ class { 'nginx::install':
   no_sendfile => true
 }
 
-class { 'php7::install': }
+package { [
+  'php7.0-fpm',
+  'php7.0-gd',
+  'php7.0-cli',
+  'php7.0-curl',
+  'php7.0-mbstring',
+  'php7.0-mysql',
+  'php-apcu',
+  'php-imagick',
+  'php-xdebug'
+  ]:
+  ensure => present
+} ->
+
+file_line { 'php_upload_max_filesize':
+  path => '/etc/php/7.0/fpm/php.ini',
+  line => 'upload_max_filesize = 200M',
+  match => '^upload_max_filesize',
+} ->
+
+file_line { 'php_post_max_size':
+  path => '/etc/php/7.0/fpm/php.ini',
+  line => 'post_max_size = 200M',
+  match => '^post_max_size',
+} ~>
+
+service { 'php7.0-fpm':
+  ensure => running,
+}
 
 class { 'mysql::server':
   root_password => lookup('mysql_root_password'),
