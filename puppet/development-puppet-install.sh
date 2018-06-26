9e1cdc259f1fda2679e29d4553740e3f38f40df5
Fredrik Ekelund <fredrik@lundgrenlindqvist.se>
2018-06-26 14:22:26 +0200

    Skip over Puppet module installation more quickly in development if modules are already installed

--- puppet/development-puppet-install.sh
+++ puppet/development-puppet-install.sh
@@ -12,7 +12,18 @@ fi
 
 export PATH=/opt/puppetlabs/bin:$PATH
 
-puppet module install puppetlabs-stdlib --target-dir /vagrant/puppet/modules --version 4.24.0
-puppet module install flypenguin-mkdir --target-dir /vagrant/puppet/modules --version 1.0.4
-puppet module install hunner-wordpress --target-dir /vagrant/puppet/modules --version 1.0.0
-puppet module install puppetlabs-mysql --target-dir /vagrant/puppet/modules --version 5.3.0
+if [ ! -d /vagrant/puppet/modules/stdlib ]; then
+  puppet module install puppetlabs-stdlib --target-dir /vagrant/puppet/modules --version 4.24.0
+fi
+
+if [ ! -d /vagrant/puppet/modules/mkdir ]; then
+  puppet module install flypenguin-mkdir --target-dir /vagrant/puppet/modules --version 1.0.4
+fi
+
+if [ ! -d /vagrant/puppet/modules/wordpress ]; then
+  puppet module install hunner-wordpress --target-dir /vagrant/puppet/modules --version 1.0.0
+fi
+
+if [ ! -d /vagrant/puppet/modules/mysql ]; then
+  puppet module install puppetlabs-mysql --target-dir /vagrant/puppet/modules --version 5.3.0
+fi
