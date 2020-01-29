b74b0a5b07d50603a501110f4610ee5411a3c22e
Fredrik Ekelund <fredrik@lundgrenlindqvist.se>
2020-01-29 16:12:44 +0100

    Install php7.2-xml

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -226,6 +226,7 @@ $production_php_packages = [
   "php${php_version}-curl",
   "php${php_version}-mbstring",
   "php${php_version}-mysql",
+  "php${php_version}-xml",
   "php${php_version}-zip",
   'php-apcu',
   'php-imagick'
